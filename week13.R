7699e6b50361a7bd11e2568466343316a8b45fda
Johnson32117 <144878793+Johnson32117@users.noreply.github.com>
2023-12-06 13:39:18 +0800

    Add files via upload

---
+++ week13.R
@@ -0,0 +1,12 @@
+oneComingStudentMajor <- sample(c("Economics", "Sociology"), 1)
+print(oneComingStudentMajor)
+
+if( oneComingStudentMajor == "Economics")
+{
+# oneComingStudentMajor == "Economics"
+print("Please go to room 201.")}
+if( oneComingStudentMajor == "Sociology")
+{
+# oneComingStudentMajor == "Sociology"
+print("Please go to room 301.")}
+
